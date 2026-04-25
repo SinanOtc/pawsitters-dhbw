b38c42118551148345475d6d707e102e44ee8820
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-04-25 13:17:23 +0200

    Spring Boot Setup

---
+++ src/main/java/dhbw/heilbronn/pawsitters/PawsittersApplication.java
@@ -0,0 +1,13 @@
+package dhbw.heilbronn.pawsitters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsittersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsittersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhbw/heilbronn/pawsitters/PawsittersApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.heilbronn.pawsitters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsittersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
