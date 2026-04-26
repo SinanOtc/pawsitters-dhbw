3ee4be34ebb85d670a2f08a68a66b7c28d69d3a8
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-04-26 20:58:00 +0200

    feat: add User entity with JPA annotations and validation constraints

---
+++ src/main/java/dhbw/heilbronn/pawsitters/domain/User.java
@@ -0,0 +1,49 @@
+package dhbw.heilbronn.pawsitters.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * User für die Anmeldung im System
+ * Profildaten liegen in Owner- bzw. HostProfile
+ */
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String passwordAberHashed;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    public User(String email, String passwordAberHashed, UserRole role) {
+        this.email = email;
+        this. passwordAberHashed = passwordAberHashed;
+        this.role = role;
+    }
+
+
+
+
+}
