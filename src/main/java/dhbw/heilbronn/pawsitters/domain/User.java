55449777caa01d05f1ab9092fbb332e0cba05790
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-05-03 16:17:01 +0200

    fix: correct typo in User class field and constructor (passwordHashed)

--- src/main/java/dhbw/heilbronn/pawsitters/domain/User.java
+++ src/main/java/dhbw/heilbronn/pawsitters/domain/User.java
@@ -31,15 +31,15 @@ public class User {
 
     @NotBlank
     @Column(nullable = false)
-    private String passwordAberHashed;
+    private String passwordHashed;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
 
-    public User(String email, String passwordAberHashed, UserRole role) {
+    public User(String email, String passwordHashed, UserRole role) {
         this.email = email;
-        this. passwordAberHashed = passwordAberHashed;
+        this. passwordHashed = passwordHashed;
         this.role = role;
     }
 }
