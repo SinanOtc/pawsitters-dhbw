42877544f1475e1fee45ba36484a2fa27d07e95f
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-05-03 20:47:50 +0200

    feat: extend OwnerProfileRepository with user query method

--- src/main/java/dhbw/heilbronn/pawsitters/repository/OwnerProfileRepository.java
+++ src/main/java/dhbw/heilbronn/pawsitters/repository/OwnerProfileRepository.java
@@ -1,6 +1,7 @@
 package dhbw.heilbronn.pawsitters.repository;
 
 import dhbw.heilbronn.pawsitters.domain.OwnerProfile;
+import dhbw.heilbronn.pawsitters.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface OwnerProfileRepository extends JpaRepository<OwnerProfile, Long> {
 
     Optional<OwnerProfile> findByUserId(Long userID);
+
+    Long user(User user);
 }
