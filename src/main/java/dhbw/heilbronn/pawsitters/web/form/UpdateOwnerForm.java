3035b10dfadac887d7c8b225521278cf74b57765
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-05-03 20:57:00 +0200

    feat: add OwnerService with registration, update, and profile retrieval logic

---
+++ src/main/java/dhbw/heilbronn/pawsitters/web/form/UpdateOwnerForm.java
@@ -0,0 +1,28 @@
+package dhbw.heilbronn.pawsitters.web.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Form für das Bearbeiten eines bestehenden Owner Profils
+ *
+ * WICHTIG:
+ *  - bei Update KEIN Passwort und Email Änderung
+ *  (das ist zu sicherheitsrelevant und muss in einen eigenen Flow)
+ */
+
+public record UpdateOwnerForm(
+
+        @NotBlank
+        @Size(max = 100)
+        String firstName,
+
+        @NotBlank
+        @Size(max = 100)
+        String lastName,
+
+        @NotBlank
+        @Size(max = 256)
+        String address
+){
+}
