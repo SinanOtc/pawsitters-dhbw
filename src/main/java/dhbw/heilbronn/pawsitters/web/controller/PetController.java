ad650ff305cace5fc79d81163b1e964bb245310d
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-05-04 19:20:28 +0200

    feat: add PetController with base mapping for /pets. To be continued

---
+++ src/main/java/dhbw/heilbronn/pawsitters/web/controller/PetController.java
@@ -0,0 +1,9 @@
+package dhbw.heilbronn.pawsitters.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pets")
+public class PetController {
+}
