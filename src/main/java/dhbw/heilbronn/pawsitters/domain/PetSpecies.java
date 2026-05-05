7e00c188c54b12c931bb61c265af9bccf9421ad3
kevalbra <kev.albrandt.24@heilbronn.dhbw.de>
2026-05-05 23:17:02 +0200

    feat: add PetSpecies enum for accepted pet types

---
+++ src/main/java/dhbw/heilbronn/pawsitters/domain/PetSpecies.java
@@ -0,0 +1,13 @@
+package dhbw.heilbronn.pawsitters.domain;
+
+/**
+ * Tierarten, die ein Host akzeptieren kann.
+ */
+public enum PetSpecies {
+    DOG,
+    CAT,
+    BIRD,
+    RABBIT,
+    REPTILE,
+    OTHER
+}
