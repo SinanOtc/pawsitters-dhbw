345f3877d31499c4204a2cfc1fc7b44a075de076
Sinan Oturucu <sin.oturucu.24@heilbronn.dhbw.de>
2026-05-19 09:54:37 +0200

    feat: enable Scheduling for CareRequestExpiry Check

--- src/main/java/dhbw/heilbronn/pawsitters/PawsittersApplication.java
+++ src/main/java/dhbw/heilbronn/pawsitters/PawsittersApplication.java
@@ -2,8 +2,14 @@ package dhbw.heilbronn.pawsitters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Scheduling aktiviert für den täglichen CareRequestExpiryScheduler.
+ * Schließt abgelaufene CareRequests täglich + einmalig beim start.
+ */
 @SpringBootApplication
+@EnableScheduling
 public class PawsittersApplication {
 
 	public static void main(String[] args) {
